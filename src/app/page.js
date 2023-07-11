daa937e808f55db5e272fe8e2c4a578ca2020052
ThaiSiang <jeremytswong@gmail.com>
2023-07-11 17:39:13 +0800

    fixed linting

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
+    <main className='flex min-h-screen flex-col items-center'>
       <div className='flex bg-gradient-to-b from-blue-500 to-blue-300 items-center flex-col justify-center w-screen h-44 '>
         <h1 className = ' font-poppins font-medium text-5xl text-white p-3 '>WaterWise</h1>
-        <p className='font-poppins text-white '>"Your solution to reducing water sustainably"</p>
+        <p className='font-poppins text-white '>Your solution to reducing water sustainably</p>
       </div>
       <div className='flex flex-row justify-center pt-5 w-screen'  > 
-      <Image  src = "/waterwisePhone.png" width={150} height={100} />
+      <Image  src = '/waterwisePhone.png' width={150} height={100} alt='image of WaterWise Splash' />
       </div>
       <div className='flex flex-col p-10'>
         <h2 className='text-black text-2xl font-poppins font-semibold'>About Us</h2>
