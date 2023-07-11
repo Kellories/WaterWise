2f2fc736e7f09f22f397a2e6ae2d6f71afd1a2b7
ThaiSiang <jeremytswong@gmail.com>
2023-07-11 17:42:05 +0800

    added quotes

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <main className='flex min-h-screen flex-col items-center'>
       <div className='flex bg-gradient-to-b from-blue-500 to-blue-300 items-center flex-col justify-center w-screen h-44 '>
         <h1 className = ' font-poppins font-medium text-5xl text-white p-3 '>WaterWise</h1>
-        <p className='font-poppins text-white '>Your solution to reducing water sustainably</p>
+        <q className='font-poppins text-white '> Your solution to reducing water sustainably </q>
       </div>
       <div className='flex flex-row justify-center pt-5 w-screen'  > 
       <Image  src = '/waterwisePhone.png' width={150} height={100} alt='image of WaterWise Splash' />
