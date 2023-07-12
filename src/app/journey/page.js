008aba5758c66547b879f73fa0d3849488a86c62
ThaiSiang <jeremytswong@gmail.com>
2023-07-12 21:13:35 +0800

    added about us text + styling

--- src/app/journey/page.js
+++ src/app/journey/page.js
@@ -1,86 +1,74 @@
 'use client'
-import { Parallax, ParallaxLayer, IParallax } from "@react-spring/parallax"
 import { useRouter } from 'next/navigation'
-import { useRef } from "react"
+
 
 const JourneyMap = () => {
-    const parallax = useRef()
     const router = useRouter()
     return (
-        <Parallax ref={parallax} pages = {9} className="bg-[url('gradientLogo.png')] bg-cover bg-center">
-            <ParallaxLayer offset = {0} speed = {1} factor = {1} style={{backgroundColor:'#FFFFFF'}}>
-                <main className='flex min-h-screen flex-col items-center font-poppins p-10 text-black' >
-                    <div className="flex flex-row justify-center items-center w-screen">
-                        <div style = {{position:'absolute', marginRight:300}}>
-                        <svg onClick={()=>{router.push('../')}} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24" >
-                            <path d="M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 11 21 L 11 15 L 13 15 L 13 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z M 12 4.7910156 L 18 10.191406 L 18 11 L 18 19 L 15 19 L 15 13 L 9 13 L 9 19 L 6 19 L 6 10.191406 L 12 4.7910156 z"></path>
-                        </svg>
-                        </div>
-                        <h1 className='text-3xl'>Journey Map</h1>
-                    </div>
-                    <div className="flex flex-col items-center justify-center pt-96 ">
+        <main>
+
+            <section className='flex min-h-screen flex-col items-center font-poppins p-10 text-black' >
+                <div className="flex flex-row justify-center items-center w-screen mb-10">
+                    <svg className='mr-3' onClick={() => { router.push('../') }} xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 24 24" >
+                        <path d="M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 11 21 L 11 15 L 13 15 L 13 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z M 12 4.7910156 L 18 10.191406 L 18 11 L 18 19 L 15 19 L 15 13 L 9 13 L 9 19 L 6 19 L 6 10.191406 L 12 4.7910156 z"></path>
+                    </svg>
+                    <h1 className='text-3xl'>Journey Map</h1>
+                </div>
+                <div className="flex flex-col items-center justify-center ">
                     <p className="text-2xl font-bold">Beginning</p>
-                        <p className="mt-10">Frugal Fiona notices that her water bills are higher than her friends. Worried about the costs, she wants to reduce it.
-                            However, she does not know the main cause of the high water bills.</p>
-                    </div>
-                </main>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1} speed = {1} factor = {3}>
-                <main className='flex min-h-screen flex-col items-center font-poppins p-10 text-black' style={{backgroundColor:'#FFFFFF'}}>
+                    <p className="">Frugal Fiona notices that her water bills are higher than her friends. Worried about the costs, she wants to reduce it.
+                        However, she does not know the section cause of the high water bills.</p>
+                </div>
+            </section>
+
+            <section className='flex min-h-screen flex-col items-center font-poppins p-10 text-black' style={{ backgroundColor: '#FFFFFF' }}>
                 <h1 className="text-3xl font-bold">Discovery</h1>
                 <p>One day, she noticed a flyer for WaterWise at her HDB block on the way home from grocery shopping.
-It claims to be a solution for managing household water usage and even has Incentives to be won. </p>
-                </main>
-            </ParallaxLayer>
-            <ParallaxLayer offset = {2} speed = {1}>
-            <main className='flex min-h-screen flex-col items-center font-poppins p-10 text-black' style={{backgroundColor:'#FFFFFF'}}>
+                    It claims to be a solution for managing household water usage and even has Incentives to be won. </p>
+            </section>
+
+            <section className='flex min-h-screen flex-col items-center font-poppins p-10 text-black' style={{ backgroundColor: '#FFFFFF' }}>
                 <h1 className="text-3xl font-bold">Exploration</h1>
                 <p>Intrigued by the information, she scans the QR code on the flyer which brings her to the App Store.
 
-She finds out about the app’s features that help to monitor water consumption and give tips to save water.
+                    She finds out about the app’s features that help to monitor water consumption and give tips to save water.
 
-It also awards vouchers to users who manage to maintain a low usage. She decides to install the app.</p>
-                </main>
-            </ParallaxLayer>
-            <ParallaxLayer offset = {3} speed = {1}>
-            <main className='flex min-h-screen flex-col items-center font-poppins p-10 text-black' style={{backgroundColor:'#FFFFFF'}}>
+                    It also awards vouchers to users who manage to sectiontain a low usage. She decides to install the app.</p>
+            </section>
+
+            <section className='flex min-h-screen flex-col items-center font-poppins p-10 text-black' style={{ backgroundColor: '#FFFFFF' }}>
                 <h1 className="text-3xl font-bold">Set Up</h1>
                 <p>Invested, Frugal Fiona sets up a Family Group and invites her family members to join. She also sets a Saving Goal and Reminders to keep the family on track.</p>
-                </main>
-            </ParallaxLayer>
-            <ParallaxLayer offset = {4} speed = {1}>
-            <main className='flex min-h-screen flex-col items-center font-poppins p-10 text-black' style={{backgroundColor:'#FFFFFF'}}>
+            </section>
+
+            <section className='flex min-h-screen flex-col items-center font-poppins p-10 text-black' style={{ backgroundColor: '#FFFFFF' }}>
                 <h1 className="text-3xl font-bold">Realisation</h1>
                 <p>On several occasions, she notices that the household receives Notifications from the app, informing them that their daily water usage limit has almost been reached. This prompts them to be more mindful of how they use water that day and also compensate by using less water the next day.
 
-Additionally, a floor plan with how much water % that unit is using may urge users to manage water usage there better.</p>
-                </main>
-            </ParallaxLayer>
-            <ParallaxLayer offset = {5} speed = {1}>
-            <main className='flex min-h-screen flex-col items-center font-poppins p-10 text-black' style={{backgroundColor:'#FFFFFF'}}>
+                    Additionally, a floor plan with how much water % that unit is using may urge users to manage water usage there better.</p>
+            </section>
+
+            <section className='flex min-h-screen flex-col items-center font-poppins p-10 text-black' style={{ backgroundColor: '#FFFFFF' }}>
                 <h1 className="text-3xl font-bold">Benefits</h1>
                 <p>Due to the family’s efforts, they were eligible for Rebates offered by WaterWise. They noticed a huge decrease in water bills due to the rebates and reduced water usage.</p>
-                </main>
-            </ParallaxLayer>
-            <ParallaxLayer offset = {6} speed = {1}>
-            <main className='flex min-h-screen flex-col items-center font-poppins p-10 text-black' style={{backgroundColor:'#FFFFFF'}}>
+            </section>
+
+            <section className='flex min-h-screen flex-col items-center font-poppins p-10 text-black' style={{ backgroundColor: '#FFFFFF' }}>
                 <h1 className="text-3xl font-bold">Benefits</h1>
                 <p>Frugal Fiona also learnt about water saving tips which contributed to the reduction of water consumption. She shared about WaterWise with all her friends in the neighbourhood.</p>
-                </main>
-            </ParallaxLayer>
-            <ParallaxLayer offset = {7} speed = {1}>
-            <main className='flex min-h-screen flex-col items-center font-poppins p-10 text-black' style={{backgroundColor:'#FFFFFF'}}>
+            </section>
+
+            <section className='flex min-h-screen flex-col items-center font-poppins p-10 text-black' style={{ backgroundColor: '#FFFFFF' }}>
                 <h1 className="text-3xl font-bold">Popularity</h1>
                 <p>Due to the app’s enticing benefits and usefulness, WaterWise soon gained popularity and became a well-known app in Singapore, used by all members of the household.</p>
-                </main>
-            </ParallaxLayer>
-            <ParallaxLayer offset = {8} speed = {1}>
-            <main className='flex min-h-screen flex-col items-center font-poppins p-10 text-black' style={{backgroundColor:'#FFFFFF'}}>
+            </section>
+
+            <section className='flex min-h-screen flex-col items-center font-poppins p-10 text-black' style={{ backgroundColor: '#FFFFFF' }}>
                 <h1 className="text-3xl font-bold">End</h1>
                 <p>Frugal Fiona and her neighbours effectively reduced their water bills and inculcated Good Water Saving Habits through the WaterWise app.</p>
-                </main>
-            </ParallaxLayer>
-        </Parallax>
+            </section>
+        </main>
+
     )
 }
 export default JourneyMap
--- src/app/page.js
+++ src/app/page.js
@@ -8,9 +8,9 @@ export default function Home() {
   return (
 
       <main className='flex min-h-screen bg-black flex-col items-center font-poppins text-white'>
-        <div className='flex items-center flex-col justify-center w-screen h-32 '>
-          <h1 className = ' font-poppins font-medium  text-transparent text-5xl bg-clip-text bg-gradient-to-r from-blue-400 to-blue-600 '>WaterWise</h1>
-          <q className='font-poppins text-white '> Your solution to reducing water sustainably </q>
+        <div className='flex items-center flex-col justify-center w-screen  '>
+          <h1 className = ' font-poppins font-medium  text-transparent text-5xl bg-clip-text bg-gradient-to-r from-blue-200 to-blue-700 mt-10 mb-2'>WaterWise</h1>
+          <q className='font-poppins text-white mb-5'> Using Water, Wiser. </q>
         </div>
         <div className='flex flex-row justify-center w-screen'  > 
         <Image  src = '/waterwisePhone.png' width={150} height={100} alt='image of WaterWise Splash' />
@@ -18,13 +18,13 @@ export default function Home() {
 
         <div className='flex flex-col px-10 pt-10'>
           <h2 className=' text-2xl font-poppins font-semibold'>About Us</h2> <br />
-          <p>WaterWise is an app with real-time water tracking paired with a smart water monitor. </p> <br />
-          <p>WaterWise solves our issue of the lack of information by having an app where people can share tips as well as access to our own curated water saving tips.</p>
+          <p>WaterWise is real-time water tracking app paired with a smart water monitor. </p> <br />
+          <p>WaterWise solves our issue of the lack of information by having an app where people can share tips as well as access to our own curated water saving tips. <br /><br />As well as letting our users find their main contributors to their household water usage, through a simple set up process and user-friendly data visualisation.</p>
           <br />
         </div>
-        <div>
-          <button onClick = {()=>{router.push('/journey')}} className='bg-blue-500 rounded-lg p-3'>See Our Journey Map</button>
-          <button></button>
+        <div className='flex flex-row'>
+          <button onClick = {()=>{router.push('/journey')}} className='bg-blue-500 rounded-lg p-3 m-2'>See Our Journey Map!</button>
+          <button className='bg-blue-500 rounded-lg p-3 m-2'> Our Persona! </button>
         </div>
       </main>
 
